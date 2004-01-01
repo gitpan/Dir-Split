12acd651c54b627af4deceb795485e9df4da886d
Steven Schubiger (stsc) <schubiger@cpan.org>
2004-01-01 01:16:10 +0000

    Import of SCHUBIGER/Dir-Split-0.32 from CPAN.
    gitpan-cpan-distribution: Dir-Split
    gitpan-cpan-version:      0.32
    gitpan-cpan-path:         SCHUBIGER/Dir-Split-0.32.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     released

---
+++ bin/split.pl
@@ -0,0 +1,64 @@
+use Dir::Split;
+
+#
+# Uncomment the lines at the bottom accordingly
+# whether numeric or characteristic splitting shall be 
+# committed. 
+#
+# Source and target dir might require some adjustment.
+#
+
+%num_behavior = (  mode    =>    'num',
+
+                   options => {  verbose        =>           1,
+                                 warn           =>       'all',
+                                 override       =>      'none',
+                   },
+
+                   sub_dir => {  identifier     =>      'test',
+                                 file_limit     =>           2,
+                                 file_sort      =>         '+',
+                   },
+
+                   suffix  => {  continue_num   =>         'y',
+                                 separator      =>         '-',
+                                 length         =>           5,
+                   },
+);
+
+%char_behavior = (  mode    =>    'char',
+
+                    options => {  verbose     =>           1,
+                                  warn        =>       'all',
+                                  override    =>      'none',
+                    },
+
+                    sub_dir => {  identifier  =>      'test',
+                    },
+
+                    suffix  => {  separator   =>         '-',
+                                  case        =>     'upper',
+                    },
+
+);
+
+$source_dir = '/tmp/src';
+$target_dir = '/tmp/destin';
+
+
+# numeric splitting
+#
+#my $dir = Dir::Split->new (\%num_behavior);
+
+# characteristic splitting
+#
+#my $dir = Dir::Split->new (\%char_behavior);
+
+# evaluate the return status and squeek accordingly
+#
+#if ($files_moved = $dir->split (\$source_dir, \$target_dir) ) {
+#    print "$files_moved files moved.\n";
+#}
+#else {
+#    print "None moved.\n";
+#}
